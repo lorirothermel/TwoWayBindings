70e4b7fc0602173111bd1ae48c8713468c425940
lorirothermel <lori@rothermel1.com>
2024-08-27 07:53:09 -0400

    Chapter 1.21 Two Way Bindings for @State

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,17 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Group {
+                Spacer()
+                
+                Text("Press Count: \(pressCount)")
+                
+                Button("Press Me") {
+                    pressCount += 1
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                TextField("Enter a name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                Text("Name entered: \(name)")
+                
+            }  // Group
+                
+            Spacer()
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            
+            ColorPicker(selection: $selectedColor, label: {
+                Text("Pick a rectangle color: ")
+            })
+            
+            Spacer()
+            
+            Toggle("Toggle is: \(toggleIsOn ? "On" : "Off")", isOn: $toggleIsOn)
+                .padding()
+            
+            Spacer()
+            
+            DatePicker("Date: ", selection: $selectedDate)
+            Text("Selected Date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Stepper Value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
+            
+        }  // VStack
+        .font(.title2)
         .padding()
-    }
-}
+        
+        
+    } // some View
+}  // ContentView
 
 #Preview {
     ContentView()
 }
+
